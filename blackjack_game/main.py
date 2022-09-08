425521a87a649d2a7bc5130924135930c8c65282
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-08 15:26:59 -0300

    random fixes

--- blackjack_game/main.py
+++ blackjack_game/main.py
@@ -136,7 +136,7 @@ class Game():
                         "Please enter 'Hit' or 'Stand' or (H/S) ").lower()
                     print()
                 if choice in ["hit", "h"]:
-                    player_hand.add_card(deck.deal())
+                    player_hand.add_card(deck.deal(1))
                     player_hand.display()
 
             if self.check_winner(player_hand, dealer_hand):
@@ -146,7 +146,7 @@ class Game():
             dealer_hand_value = dealer_hand.get_value()
 
             while dealer_hand_value < 17:
-                dealer_hand.add_card(deck.deal())
+                dealer_hand.add_card(deck.deal(1))
                 dealer_hand_value = dealer_hand.get_value()
 
             dealer_hand.display(show_all_dealer_cards=True)
